5f445d172e30eb5db36e4e2b5c89629b01538d53
Gastón Caruso <gstn.caruso@gmail.com>
2024-07-21 20:35:44 -0300

    display missing field error after submit if value is empty

--- src/components/payment_form/PaymentInformationFormSection.tsx
+++ src/components/payment_form/PaymentInformationFormSection.tsx
@@ -1,25 +1,58 @@
-import { FormEvent }            from 'react';
-import { SubmitButton }         from '../SubmitButton';
-import { FormInput }            from './form_input/FormInput';
-import { FormStepSectionTitle } from './FormStepSectionTitle';
+import { ChangeEventHandler, FormEvent, useState } from 'react';
+import { SubmitButton }                            from '../SubmitButton';
+import { FormInput }                               from './form_input/FormInput';
+import { FormStepSectionTitle }                    from './FormStepSectionTitle';
+
+interface PaymentFormContentInformation {
+  cardNumber: string;
+  expires: string;
+  cvv: string;
+  holderName: string;
+  zip: string;
+}
 
 export function PaymentInformationFormSection() {
-  function handleSubmit(event: FormEvent) {
+  const blankFormInformation: PaymentFormContentInformation = {
+    cardNumber: '',
+    expires: '',
+    cvv: '',
+    holderName: '',
+    zip: ''
+  };
+  const [formContent, setFormContent] = useState<PaymentFormContentInformation>(blankFormInformation);
+  const [firstRender, hasSubmitted] = useState(true);
 
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    console.log(formContent);
+    hasSubmitted(false);
   }
+
+  const handleChange: ChangeEventHandler = (event) => {
+    event.preventDefault();
+    // @ts-ignore
+    const { value, name } = event.target;
+    setFormContent(prevState => ({ ...prevState, [name]: value }));
+  };
 
   return <div className="PaymentInformationFormSection">
     <FormStepSectionTitle sectionNumber={ 1 } title="Payment Information"/>
-    <form onSubmit={ handleSubmit }>
-      <FormInput type={ 'text' } title={ 'Card number' }/>
-      <div className="column">
-        <FormInput type={ 'text' } title={ 'Expires (MM/YY)' }/>
-        <FormInput type={ 'text' } title={ 'Security code (CVV)' }/>
-      </div>
-      <FormInput type={ 'text' } title={ 'Name on card' }/>
-      <FormInput type={ 'text' } title={ 'ZIP code' }/>
 
-      <SubmitButton onClick={ handleSubmit } label={"Continue"}/>
+    <form onSubmit={ handleSubmit }>
+      <FormInput name={ 'cardNumber' } type={ 'text' } title={ 'Card number' } value={ formContent.cardNumber } onChange={ handleChange }
+                 isMissing={ !firstRender && !formContent.cardNumber }/>
+      <div className="column">
+        <FormInput name={ 'expires' } value={ formContent.expires } type={ 'text' } title={ 'Expires (MM/YY)' } onChange={ handleChange }
+                   isMissing={ !firstRender && !formContent.expires }/>
+        <FormInput name={ 'cvv' } value={ formContent.cvv } type={ 'text' } title={ 'Security code (CVV)' } onChange={ handleChange }
+                   isMissing={ !firstRender && !formContent.cvv }/>
+      </div>
+      <FormInput name={ 'holderName' } value={ formContent.holderName } type={ 'text' } title={ 'Name on card' } onChange={ handleChange }
+                 isMissing={ !firstRender && !formContent.holderName }/>
+      <FormInput name={ 'zip' } value={ formContent.zip } type={ 'text' } title={ 'ZIP code' } onChange={ handleChange }
+                 isMissing={ !firstRender && !formContent.zip }/>
+
+      <SubmitButton onClick={ handleSubmit } label={ 'Continue' }/>
     </form>
   </div>;
 }
--- src/components/payment_form/form_input/FormInput.tsx
+++ src/components/payment_form/form_input/FormInput.tsx
@@ -1,15 +1,18 @@
-import { HTMLInputTypeAttribute } from 'react';
+import { ChangeEventHandler, HTMLInputTypeAttribute } from 'react';
 
 interface FormInputProps {
   title: string,
   type: HTMLInputTypeAttribute,
   isMissing?: boolean,
+  value?: string,
+  onChange: ChangeEventHandler,
+  name: string,
 }
 
-export function FormInput({ title, type, isMissing }: FormInputProps) {
+export function FormInput({ name, title, type, isMissing, value, onChange }: FormInputProps) {
   return <label className={ `FormInput` }>
     { title }
-    <input className={ `${ isMissing ? 'error' : '' }` } type={ type } required></input>
+    <input name={name} className={ `${ isMissing ? 'error' : '' }` } type={ type } required value={value} onChange={onChange}></input>
 
     { isMissing && <p>This field is required.</p> }
   </label>;
