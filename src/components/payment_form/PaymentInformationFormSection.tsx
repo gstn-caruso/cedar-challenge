71d90dc9c4a0229200c95f316e6aa242e6947209
Gastón Caruso <gstn.caruso@gmail.com>
2024-07-21 20:54:44 -0300

    hides payment information form after submit

--- src/components/payment_form/PaymentInformationFormSection.tsx
+++ src/components/payment_form/PaymentInformationFormSection.tsx
@@ -47,21 +47,23 @@ export function PaymentInformationFormSection(props: PaymentFormContentInformati
   return <div className="PaymentInformationFormSection">
     <FormStepSectionTitle sectionNumber={ 1 } title="Payment Information" isActive={ props.isActive }/>
 
-    <form onSubmit={ handleSubmit }>
-      <FormInput name={ 'cardNumber' } type={ 'text' } title={ 'Card number' } value={ formContent.cardNumber } onChange={ handleChange }
-                 isMissing={ !firstRender && !formContent.cardNumber }/>
-      <div className="column">
-        <FormInput name={ 'expires' } value={ formContent.expires } type={ 'text' } title={ 'Expires (MM/YY)' } onChange={ handleChange }
-                   isMissing={ !firstRender && !formContent.expires }/>
-        <FormInput name={ 'cvv' } value={ formContent.cvv } type={ 'text' } title={ 'Security code (CVV)' } onChange={ handleChange }
-                   isMissing={ !firstRender && !formContent.cvv }/>
-      </div>
-      <FormInput name={ 'holderName' } value={ formContent.holderName } type={ 'text' } title={ 'Name on card' } onChange={ handleChange }
-                 isMissing={ !firstRender && !formContent.holderName }/>
-      <FormInput name={ 'zip' } value={ formContent.zip } type={ 'text' } title={ 'ZIP code' } onChange={ handleChange }
-                 isMissing={ !firstRender && !formContent.zip }/>
+    { props.isActive &&
+        <form onSubmit={ handleSubmit }>
+            <FormInput name={ 'cardNumber' } type={ 'text' } title={ 'Card number' } value={ formContent.cardNumber } onChange={ handleChange }
+                       isMissing={ !firstRender && !formContent.cardNumber }/>
+            <div className="column">
+                <FormInput name={ 'expires' } value={ formContent.expires } type={ 'text' } title={ 'Expires (MM/YY)' } onChange={ handleChange }
+                           isMissing={ !firstRender && !formContent.expires }/>
+                <FormInput name={ 'cvv' } value={ formContent.cvv } type={ 'text' } title={ 'Security code (CVV)' } onChange={ handleChange }
+                           isMissing={ !firstRender && !formContent.cvv }/>
+            </div>
+            <FormInput name={ 'holderName' } value={ formContent.holderName } type={ 'text' } title={ 'Name on card' } onChange={ handleChange }
+                       isMissing={ !firstRender && !formContent.holderName }/>
+            <FormInput name={ 'zip' } value={ formContent.zip } type={ 'text' } title={ 'ZIP code' } onChange={ handleChange }
+                       isMissing={ !firstRender && !formContent.zip }/>
 
-      <SubmitButton onClick={ handleSubmit } label={ 'Continue' }/>
-    </form>
+            <SubmitButton onClick={ handleSubmit } label={ 'Continue' }/>
+        </form>
+    }
   </div>;
 }
