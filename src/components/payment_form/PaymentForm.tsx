2471d31c13806293f4ec75de5182d1c0fc273c0e
Gastón Caruso <gstn.caruso@gmail.com>
2024-07-21 18:43:47 -0300

    style PaymentForm

--- src/components/payment_form/PaymentForm.tsx
+++ src/components/payment_form/PaymentForm.tsx
@@ -2,7 +2,7 @@ import { PaymentInformationFormSection } from './PaymentInformationFormSection';
 import { ReviewAndPayFormSection }       from './ReviewAndPayFormSection';
 
 export function PaymentForm() {
-  return <div data-testid="payment-form">
+  return <div data-testid="payment-form" className="PaymentForm">
     <PaymentInformationFormSection />
     <ReviewAndPayFormSection />
   </div>;
