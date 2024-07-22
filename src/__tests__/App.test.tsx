43ffb247bf4a893c2b0231476026f04630106818
Gastón Caruso <gstn.caruso@gmail.com>
2024-07-22 11:22:24 -0300

    refactor tests

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -4,14 +4,15 @@ import App                           from '../App';
 
 test('it renders welcome message', () => {
   render(<App/>);
-  const welcomeMessage = screen.getByText(/Hi, Taylor/i);
-  expect(welcomeMessage).toBeInTheDocument();
+
+  expectScreenContainsElementWithText('Hi, Taylor');
 });
 
 test('it renders Payment information form after clicking pay button', () => {
   render(<App/>);
-  const payButton = screen.getByText(/Pay total/i);
-  fireEvent.click(payButton);
+
+  clickButtonWithText('Pay total')
+
   const paymentForm = screen.getByTestId('payment-form');
   expect(paymentForm).toBeInTheDocument();
 })
@@ -19,32 +20,38 @@ test('it renders Payment information form after clicking pay button', () => {
 test('it renders thank you page after submit valid information', () => {
   render(<App/>);
 
-  const payButton = screen.getByText(/Pay total/i);
-  fireEvent.click(payButton);
+  clickButtonWithText('Pay total')
 
   fillInput('cardNumber', '4242424242424242');
   fillInput('expires', '12/24');
   fillInput('cvv', '123');
+  fillInput('holderName', 'Test User');
+  fillInput('zip', '123');
 
-  const cardHolderName = screen.getByTestId('holderName');
-  fireEvent.change(cardHolderName, { target: { value: '123' } });
+  clickButtonWithText('Continue');
 
-  const zipCode = screen.getByTestId('zip');
-  fireEvent.change(zipCode, { target: { value: '123' } });
+  expectScreenContainsElementWithText('Card ending in ••••4242');
 
-  const continueButton = screen.getByText(/Continue/i);
-  fireEvent.click(continueButton);
+  clickButtonWithRegex(/Pay \$600.00/i);
 
-  const reviewAndPay = screen.getByText('Card ending in ••••4242');
-  expect(reviewAndPay).toBeInTheDocument();
-
-  const submitButton = screen.getByText(/Pay \$600.00/i);
-  fireEvent.click(submitButton);
-
-  expect(screen.getByText('Thank you for your payment!')).toBeInTheDocument();
+  expectScreenContainsElementWithText('Thank you for your payment!');
 })
+
+function clickButtonWithRegex(regex: RegExp) {
+  const continueButton = screen.getByText(regex);
+  fireEvent.click(continueButton);
+}
+
+function clickButtonWithText(buttonText: string) {
+  clickButtonWithRegex(new RegExp(buttonText));
+}
 
 function fillInput(inputTestId: string, value: string) {
   const cardNumberField = screen.getByTestId(inputTestId);
   fireEvent.change(cardNumberField, { target: { value: value } });
+}
+
+function expectScreenContainsElementWithText(expectedText: string) {
+  const reviewAndPay = screen.getByText(expectedText);
+  expect(reviewAndPay).toBeInTheDocument();
 }
